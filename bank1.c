78328a0914ee744c9ae805a6de2fa56b3008aa60
OmerDiaaeldin <omardiaaeldin2592001@gmail.com>
2024-05-09 12:02:50 +0100

    better version

--- bank1.c
+++ bank1.c
@@ -18,13 +18,14 @@ void update(char *shm, int amount){
 }
 
 int main(){
-    sem_t *s1;
-    s1 = sem_open("/first", O_EXCL);
+    sem_t *s1,*s2;
+    s1 = sem_open("/first", O_CREAT, 0644, 0);
+    s2 = sem_open("/second", O_CREAT, 0644,0);
     int shmid;
     int key = SHM_KEY;
     char *shm;
     /* create shared memory segment */
-    shmid = shmget(key, SHM_SIZE, 0666);
+    shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666);
     if (shmid < 0) {
         perror("shmget");
         exit(1);
@@ -43,4 +44,7 @@ int main(){
     // printf("%d\n", x);
     sem_wait(s1);
     update(shm,20);
+
+    sem_close(s1);
+    sem_close(s2);
 }
